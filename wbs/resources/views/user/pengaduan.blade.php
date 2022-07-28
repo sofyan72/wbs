5d38cb5b10394cb5dce5efa64abce201876c1821
Ahmad Sofyan .A <sofyan7273@gmail.com>
2022-07-28 10:32:04 +0700

    pengaduan user

---
+++ wbs/resources/views/user/pengaduan.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Inria+Sans&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="v369_1877"><span class="v369_1878">Tulis PENGADUAN</span><span class="v369_1879">Apabila Anda mendapati
+            adanya pelanggaran di lingkungan Pemerintah Kota Malang, laporkan pada Inspektorat Kota Malang. Laporan Anda
+            akan ditindaklanjuti apabila sesuai
+            dengan syarat/kriteria Laporan. Anda tidak perlu khawatir terungkapnya identitas diri anda karena
+            Inspektorat Kota Malang akan MERAHASIAKAN & MELINDUNGI Identitas Anda
+            sebagai whistleblower. Kami sangat menghargai informasi yang Anda laporkan. Fokus kami kepada materi
+            informasi yang Anda Laporkan. Sebagai bentuk terimakasih kami terhadap
+            laporan yang Anda kirim, kami berkomitmen untuk merespon laporan Anda selambat-lambatnya 15 (Lima Belas)
+            hari kerja sejak laporan Anda dikirim.</span>
+        <div class="name"></div>
+        <div class="v369_1891"></div><span class="v369_1892">© 2021 Pemerintah Kota Malang - All Rights Reserved</span>
+        <div class="name"></div>
+        <div class="v369_1894">
+            <div class="v369_1895"></div><span class="v369_1896">Login Disini</span><span class="v369_1897">Masukkan
+                kode keamanan diatas:</span>
+            <div class="v369_1898"></div><span class="v369_1899">**KTP/SIM/Identitas pengenal lainnya</span><span
+                class="v369_1900">Pilih file</span><span class="v369_1901">Pilih file</span><span
+                class="v369_1902">Bukti Pendukung</span><span class="v369_1903">Kode Keamanan</span><span
+                class="v369_1904">Buat Akun</span><span class="v369_1905">Lengkapi Identitas Anda</span><span
+                class="v369_1906">Detail Pengaduan</span><span class="v369_1907">Buat akun untuk memantau pengaduan
+                Anda. Jika Anda sudah pernah membuat akun, silahkan sebelum membuat pengaduan baru.</span><span
+                class="v369_1908">Lengkapi identitas diri Anda untuk mempermudah Tim Whistleblowing System melakukan
+                verifikasi data atas pengaduan Anda. Pengaduan dengan identitas
+                pelapor yang tidak jelas tidak dapat ditindaklanjuti.</span><span class="v369_1909">Lengkapi seluruh
+                form berikut. Sampaikan laporan Anda secara detail dan jelas.</span><span
+                class="v369_1910">1</span><span class="v369_1911">2</span><span class="v369_1912">3</span>
+            <div class="v369_1913"></div>
+            <div class="v369_1914"></div>
+            <div class="v369_1915"></div>
+            <div class="v369_1916"></div>
+            <div class="v369_1917"></div>
+            <div class="v369_1918"></div>
+            <div class="v369_1919"></div>
+            <div class="v369_1920"></div>
+            <div class="v369_1921"></div>
+            <div class="v369_1922"></div>
+            <div class="v369_1923"></div>
+            <div class="v369_1924"></div>
+            <div class="v369_1925"></div>
+            <div class="v369_1926"></div>
+            <div class="v369_1927"></div>
+            <div class="v369_1928"></div><span class="v369_1929">Username</span><span
+                class="v369_1930">Username</span><span class="v369_1931">Username</span><span class="v369_1932">Nama
+                Lengkap</span><span class="v369_1933">Nama Lengkap</span><span class="v369_1934">Nama Pihak yang
+                terlapor</span><span class="v369_1935">Nama Terlapor</span><span class="v369_1936">Password</span><span
+                class="v369_1937">Password</span><span class="v369_1938">Password</span><span class="v369_1939">NIK
+                (Nomor Induk Kependudukan)</span><span class="v369_1940">NIK </span><span class="v369_1941">Jabatan
+                Pihak yang terlapor</span><span class="v369_1942">Perangkat Daerah yang terlapor</span><span
+                class="v369_1943">Jabatan</span><span class="v369_1944">Perangkat Daerah</span><span
+                class="v369_1945">Ketik Ulang Password</span><span class="v369_1946">Konfirmasi Password</span><span
+                class="v369_1947">Konfirmasi Password</span><span class="v369_1948">Alamat E-mail</span><span
+                class="v369_1949">E-mail</span><span class="v369_1950">Tempat kejadian</span><span
+                class="v369_1951">Tempat </span><span class="v369_1952">Tanggal kejadian</span><span
+                class="v369_1953">Masukkan kode keamanan (6 Karakter)</span><span class="v369_1954">Tanggal </span><span
+                class="v369_1955">Kejadian</span><span class="v369_1956">Detail kejadian yang dilaporkan</span><span
+                class="v369_1957">No. Telp </span><span class="v369_1958">Kota/Kabupaten/Provinsi</span><span
+                class="v369_1959">No. Telp </span><span class="v369_1960">Asal Pelapor</span><span
+                class="v369_1961">Alamat Rumah</span><span class="v369_1962">Alamat Rumah</span><span
+                class="v369_1963">Upload Tanda pengenal</span>
+            <div class="v369_1964"></div>
+            <div class="v369_1965"></div>
+            <div class="name"></div>
+            <div class="v369_1967"></div>
+            <div class="v369_1968">
+                <div class="v369_1969"></div>
+            </div>
+            <div class="v369_1970"></div>
+            <div class="v369_1971"></div><span class="v369_1972">Sekretariat DPRD</span><span
+                class="v369_1973">Inspektorat</span><span class="v369_1974">Badan Pengelolaan Keuangan dan Pajak
+                Daerah</span>
+            <div class="v369_1975"></div><span class="v369_1976">Ketik nama Perangkat Daerah disini ...</span>
+            <div class="name"></div>
+            <div class="v369_1978">
+                <div class="name"></div>
+            </div>
+            <div class="v369_1986">
+                <div class="v369_1987"></div>
+            </div>
+            <div class="name"></div>
+        </div>
+        <div class="v369_1992">
+            <div class="name"></div>
+        </div>
+        <div class="v369_1997">
+            <div class="name"></div>
+        </div>
+        <div class="v369_2002">
+            <div class="v369_2003"></div>
+        </div><span class="v369_2004">Inspektorat Kota Malang</span><span class="v369_2005">Jl. Gajahmada No.2A,
+            Kiduldalem, Kec. Klojen,
+            Kota Malang, Jawa Timur 65119</span><span class="v369_2006">No. Telp</span><span
+            class="v369_2007">Website</span>
+        <div class="v369_2008">
+            <div class="name"></div>
+        </div><span class="v369_2012">Email</span><span
+            class="v369_2013">whistleblower.inspektorat@malangkota.go.id</span><span
+            class="v369_2014">0341364450</span><span class="v369_2015">whistleblower.inspektoratmalangkota.go.id</span>
+        <div class="name"></div>
+    </div>
+</body>
+
+</html> <br /><br/>
