8461904c8dc085d1f96321ee245f2e4d894a4daa
sanggaasmara <sanggaasmara@gmail.com>
2022-07-28 11:06:05 +0700

    login user

---
+++ wbs/resources/views/user/login.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="v369_2564"><span class="v369_2565">Pantau PENGADUAN</span>
+        <div class="name"></div><span class="v369_2567">Kami sangat menghargai informasi yang Anda laporkan. Fokus kami
+            kepada materi informasi yang Anda Laporkan. Untuk memantau progress tindak lanjut atas
+            pengaduan Anda, silahkan memasukkan username dan kata sandi Anda pada form login dibawah ini. Jika Anda
+            belum memiliki Akun,
+            silahkan Buat Akun dan Tulis Pengaduan.</span>
+        <div class="v369_2568"></div>
+        <div class="v369_2569">
+            <div class="v369_2570">
+                <div class="v369_2571"></div><span class="v369_2572">Username</span><span
+                    class="v369_2573">Password</span><span class="v369_2574">LOGIN</span>
+                <div class="v369_2575"></div>
+                <div class="v369_2576"></div><span class="v369_2577">Masukkan username anda</span><span
+                    class="v369_2578">Masukkan password anda</span><span class="v369_2579">Masukkan kode keamanan
+                    diatas:</span>
+                <div class="v369_2580"></div><span class="v369_2581">Kode Keamanan</span>
+                <div class="v369_2582"></div><span class="v369_2583">Masukkan kode keamanan (6 Karakter)</span>
+                <div class="v369_2584">
+                    <div class="name"></div>
+                </div>
+            </div>
+            <div class="v369_2592"></div>
+            <div class="v369_2593"><span class="v369_2594">LOGIN</span>
+                <div class="v369_2595">
+                    <div class="name"></div>
+                    <div class="v369_2600"></div>
+                    <div class="name"></div>
+                    <div class="v369_2605"></div>
+                </div>
+            </div>
+        </div>
+        <div class="v369_2606"></div>
+        <div class="v369_2607">
+            <div class="v369_2608"></div><span class="v369_2609">© 2021 Pemerintah Kota Malang - All Rights
+                Reserved</span>
+        </div>
+        <div class="v369_2610">
+            <div class="v369_2611">
+                <div class="v369_2612"><span class="v369_2613">LUPA PASSWORD</span></div>
+                <div class="v369_2614">
+                    <div class="v369_2615"></div>
+                    <div class="v369_2616"></div>
+                    <div class="v369_2617"></div>
+                </div>
+            </div>
+            <div class="v369_2618"></div>
+        </div>
+        <div class="v369_2619"></div>
+        <div class="v369_2620">
+            <div class="v369_2621"></div>
+            <div class="v369_2622"></div>
+            <div class="v369_2623">
+                <div class="v369_2624"></div>
+            </div>
+        </div>
+        <div class="name"></div>
+    </div>
+</body>
+
+</html> <br /><br />
