5ab044caf00b4c869b3158f714f9a537f5eb6c17
Rezaldy Rafi <rezaldy.rafi2@gmail.com>
2022-07-28 10:12:55 +0700

    dashboard user

---
+++ wbs/resources/views/user/dashboard.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link
+            href="https://fonts.googleapis.com/css?family=Poppins&display=swap"
+            rel="stylesheet"
+        />
+        <link
+            href="https://fonts.googleapis.com/css?family=Inria+Sans&display=swap"
+            rel="stylesheet"
+        />
+    </head>
+    <body>
+        <div class="v369_1583">
+            <div class="v369_1584">
+                <div class="v369_1585"></div>
+                <div class="v369_1586"></div>
+                <div class="v369_1587"></div>
+                <div class="name"></div>
+                <span class="v369_1598"
+                    >Mari bersama-sama menciptakan Pemerintahan yang jujur dan
+                    bersih. Laporkan setiap pelanggaran yang terjadi di
+                    lingkungan kerja Pemerintah Kota Malang.</span
+                >
+                <div class="name"></div>
+                <span class="v369_1600">Beranda</span>
+                <span class="v369_1601">Informasi</span>
+                <div class="v369_1602"></div>
+                <span class="v369_1603">Pantau Pengaduan</span>
+            </div>
+            <div class="v369_1604"></div>
+            <span class="v369_1605">Definisi WhistleBlowing System</span>
+            <span class="v369_1606"
+                >Sistem pelaporan pelanggaran mekanisme penyampaian pengaduan
+                dugaan tindak pidana korupsi yang terjadi yang melibatkan
+                pegawai dan orang lain yang berkaitan dengan dugaan tindak
+                pidana korupsi yang dilakukan di dalam organisasi tempatnya
+                bekerja.
+            </span>
+            <span class="v369_1607">Tata cara pengaduan</span>
+            <span class="v369_1608"
+                >Tata Cara Membuat Pengaduan Melalui WBS</span
+            >
+            <div class="v369_1609"></div>
+            <div class="v369_1610"></div>
+            <div class="v369_1611"></div>
+            <div class="v369_1612"></div>
+            <span class="v369_1613">1</span>
+            <span class="v369_1614">2</span>
+            <span class="v369_1615">3</span>
+            <span class="v369_1616">4</span
+            ><span class="v369_1617"
+                >Kumpulkan data dan Bukti-bukti untuk laporan berupa data,
+                dokumen, gambar, dan/atau rekaman yang mendukung/menjelaskan
+                adanya Tindak Pidana Korupsi.</span
+            >
+            <span class="v369_1618"
+                >Laporkan melalui web WBS Pemerintah Kota Malang:
+                https://wbs.malang.go.id</span
+            ><span class="v369_1619"
+                >Pelapor mendapatkan akun untuk memantau pengaduan dan
+                berkomunikasi dengan tim Verifikator Whistlebowling System</span
+            >
+            <div class="v369_1620">
+                <div class="name"></div>
+                <div class="name"></div>
+                <div class="name"></div>
+            </div>
+            <div class="v369_1624">
+                <div class="name"></div>
+                <div class="name"></div>
+                <div class="name"></div>
+            </div>
+            <div class="v369_1628">
+                <div class="name"></div>
+                <div class="name"></div>
+                <div class="name"></div>
+            </div>
+            <span class="v369_1632"
+                >Selesai. Pengaduan diproses. Pelapor dapat memantau progress
+                tindak lanjut pengaduan dengan akun yang telah diberikan</span
+            >
+            <div class="v369_1633"></div>
+            <div class="v369_1634"></div>
+            <div class="v369_1635"></div>
+            <div class="v369_1636"></div>
+            <div class="v369_1637"></div>
+            <span class="v369_1638">0</span><span class="v369_1639">15</span
+            ><span class="v369_1640">20</span><span class="v369_1641">25</span
+            ><span class="v369_1642">30</span><span class="v369_1643">35</span
+            ><span class="v369_1644">40</span><span class="v369_1645">45</span
+            ><span class="v369_1646">Pengajuan diterima</span
+            ><span class="v369_1647">Pengajuan diproses</span
+            ><span class="v369_1648">Pengajuan ditolak</span
+            ><span class="v369_1649"
+                >Statistik Pengaduan Pada WBS Pemerintah Kota Malang</span
+            ><span class="v369_1650">STATISTIK PENGADUAN</span>
+            <div class="v369_1651"></div>
+            <div class="v369_1652"></div>
+            <div class="v369_1653"></div>
+            <div class="v369_1654"></div>
+            <div class="v369_1655"></div>
+            <div class="v369_1656"></div>
+            <div class="v369_1657"></div>
+            <div class="v369_1658"></div>
+            <div class="v369_1659"></div>
+            <div class="v369_1660"></div>
+            <div class="v369_1661"></div>
+            <div class="v369_1662"></div>
+            <div class="v369_1663"></div>
+            <div class="name"></div>
+            <div class="name"></div>
+            <div class="name"></div>
+        </div>
+    </body>
+</html>
