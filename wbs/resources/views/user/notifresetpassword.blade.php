b06253e86e03a90d60f151e63bd65c972609060d
sanggaasmara <sanggaasmara@gmail.com>
2022-07-29 11:20:49 +0700

    user

---
+++ wbs/resources/views/user/notifresetpassword.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="v369_2642">
+        <div class="v369_2643"></div><span class="v369_2644">Password berhasil di reset !
+            Periksa e-mail anda</span>
+        <div class="v369_2645"></div><span class="v369_2646">Oke</span>
+        <div class="v369_2647">
+            <div class="name"></div>
+        </div>
+    </div>
+</body>
+
+</html> <br /><br />
