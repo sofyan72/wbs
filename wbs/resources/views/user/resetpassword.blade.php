8bec849efb8f1c70b1135bc9b85329b2afad7e96
sanggaasmara <sanggaasmara@gmail.com>
2022-07-29 11:13:24 +0700

    reset pas

---
+++ wbs/resources/views/user/resetpassword.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="v369_2626">
+        <div class="name"></div>
+        <div class="v369_2628"></div>
+        <div class="v369_2629">
+            <div class="v369_2630"></div><span class="v369_2631">E-Mail</span><span class="v369_2632">RESET
+                PASSWORD</span>
+            <div class="v369_2633"></div><span class="v369_2634">Masukkan email yang terdaftar di sistem ini</span>
+            <div class="v369_2635"></div>
+            <div class="v369_2636"><span class="v369_2637">RESET PASSWORD</span></div>
+        </div>
+        <div class="v369_2638">
+            <div class="v369_2639"></div><span class="v369_2640">© 2021 Pemerintah Kota Malang - All Rights
+                Reserved</span>
+        </div>
+        <div class="name"></div>
+    </div>
+</body>
+
+</html> <br /><br />
