d5217cc2550cb3184e2b4808c245712770fea4a5
sanggaasmara <sanggaasmara@gmail.com>
2022-08-01 14:25:24 +0700

    admin

---
+++ wbs/resources/views/admin/login.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="v121_434">
+        <div class="name"></div>
+        <div class="v121_436"></div>
+        <div class="v128_643"></div>
+        <div class="v128_644"></div>
+        <div class="v286_1353"></div>
+        <div class="v121_437"></div><span class="v121_438">Login </span>
+        <div class="v121_440"></div>
+        <div class="v133_1063"></div>
+        <div class="v121_441"></div><span class="v121_442">Masukkan ID</span><span class="v133_1064">Reset PASSWORD_BCRYPT
+            
+        </span><span class="v121_443">Masukkan Password</span>
+        <div class="v128_645"></div><span class="v128_646">Login</span>
+        <div class="v133_1065">
+            <div class="v133_1066"></div>
+            <div class="v133_1067"></div>
+        </div>
+        <div class="v133_1068">
+            <div class="name"></div>
+        </div>
+        <div class="v133_1074">
+            <div class="v133_1075"></div>
+            <div class="v133_1076"></div>
+            <div class="v133_1077">
+                <div class="v133_1078"></div>
+                <div class="v133_1079"></div>
+            </div>
+        </div>
+        <div class="name"></div>
+        <div class="name"></div>
+        <div class="v133_1082"></div>
+        <div class="v133_1083"></div><span class="v133_1084">Lupa Password</span><span class="v133_1085">Daftar
+            Akun</span><span class="v285_1348">WhistleBlowing System
+            Pemerintah Kota Malang</span>
+        <div class="v285_1341"></div>
+        <div class="v285_1342">
+            <div class="v285_1343"></div>
+            <div class="v285_1344"></div>
+        </div>
+        <div class="v285_1345"></div>
+        <div class="v285_1346"></div>
+        <div class="v285_1347"></div>
+    </div>
+</body>
+
+</html> <br /><br />
---
+++ wbs/resources/views/admin/register.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="v199_728">
+        <div class="name"></div>
+        <div class="v199_730"></div>
+        <div class="v199_731"></div>
+        <div class="v199_732"></div>
+        <div class="v286_1357"></div>
+        <div class="v286_1358">
+            <div class="v199_733"></div><span class="v199_734">Daftar Akun</span>
+            <div class="v199_736"></div>
+            <div class="v199_737"></div>
+            <div class="v202_692"></div>
+            <div class="v202_705"></div><span class="v199_738">Nama</span><span class="v199_739">NIP </span><span
+                class="v202_693">Email aktif</span><span class="v202_706">No. HP Aktif</span>
+            <div class="v199_740"></div><span class="v199_741">Daftar</span>
+            <div class="v199_742">
+                <div class="v199_743"></div>
+                <div class="v199_744"></div>
+            </div>
+            <div class="name"></div>
+            <div class="v202_686">
+                <div class="v202_687"></div>
+                <div class="v202_688"></div>
+                <div class="v202_689"></div>
+                <div class="v202_690"></div>
+                <div class="v202_691"></div>
+            </div>
+            <div class="v202_700">
+                <div class="name"></div>
+            </div>
+            <div class="v202_712">
+                <div class="v202_713"></div>
+            </div>
+            <div class="v202_723"></div><span class="v202_726">LOGIN</span>
+            <div class="v303_1645"><span class="v303_1646">WhistleBlowing System
+                    Pemerintah Kota Malang</span>
+                <div class="v303_1647"></div>
+                <div class="v303_1648">
+                    <div class="v303_1649"></div>
+                    <div class="v303_1650"></div>
+                </div>
+                <div class="v303_1651"></div>
+                <div class="v303_1652"></div>
+                <div class="v303_1653"></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html> <br /><br />
