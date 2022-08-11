7329788d12ef63db051041f41a8da6e40d19bebb
Rezaldy Rafi <rezaldy.rafi2@gmail.com>
2022-08-11 13:59:51 +0700

    migrate admin user

---
+++ wbs/database/migrations/2022_08_11_025749_create_user_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('password');
+            $table->string('nama');
+            $table->string('nik')->unique();
+            $table->string('email');
+            $table->string('telp');
+            $table->string('alamat');
+            $table->string('asal');
+            $table->string('ktp');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+};
