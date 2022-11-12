6d84df489ab485cdbd003fcce17f85b09c8bc98e
Rezaldy Rafi <rezaldy.rafi2@gmail.com>
2022-11-12 22:39:51 +0700

    login page

--- wbs/resources/views/admin/login.blade.php
+++ wbs/resources/views/admin/login.blade.php
@@ -1,70 +1,119 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="login.css" />
-
-    <title>Login admin</title>
-  </head>
-  <body>
-	<div class="navbar navbar-expand-lg py-4" style="background-image: url('assets/img/shapes/bg.svg'); width: 1920px; height: 1080px;">
-		
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="panel panel-default">
-                    <div class="panel-heading"> <strong class="">Login</strong>
-      
-                    </div>
-                    <div class="panel-body">
-                        <form class="form-horizontal" role="form">
-                            <div class="form-group">
-                                <label for="inputEmail3" class="col-sm-3 control-label">Email</label>
-                                <div class="col-sm-9">
-                                    <input class="form-control" id="inputEmail3" placeholder="Email" required="" type="email">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputPassword3" class="col-sm-3 control-label">Password</label>
-                                <div class="col-sm-9">
-                                    <input class="form-control" id="inputPassword3" placeholder="Password" required="" type="password">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="col-sm-offset-3 col-sm-9">
-                                    <div class="checkbox">
-                                        <label class="">
-                                            <input class="" type="checkbox">Remember me</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group last">
-                                <div class="col-sm-offset-3 col-sm-9">
-                                    <button type="submit" class="btn btn-success btn-sm">Sign in</button>
-                                    <button type="reset" class="btn btn-default btn-sm">Reset</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="panel-footer">Not Registered? <a href="#" class="">Register here</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-      </div>
-	</div>
-      
-          <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-          <!-- Include all compiled plugins (below), or include individual files as needed -->
-          <script src="js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-   
-  </body>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+		<title>Document</title>
+		<link href="assets/css/admin.css" rel="stylesheet" />
+		<!-- Fonts -->
+		<link rel="preconnect" href="https://fonts.googleapis.com" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link
+			href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+			rel="stylesheet"
+		/>
+	</head>
+	<body
+		class="bg-login"
+		style="background-image: url(assets/img/background.jpg)"
+	>
+		<div class="bg-overlay min-vh-100">
+			<div
+				class="container"
+				style="
+					position: absolute;
+					left: 50%;
+					top: 50%;
+					transform: translate(-50%, -50%);
+				"
+			>
+				<div class="d-flex justify-content-center">
+					<div class="login-box">
+						<div
+							class="card card-login pt-4 pb-5 position-relative"
+						>
+							<div class="bg-gradient-hexagon"></div>
+							<div class="text-center">
+								<a href="beranda.html" class="brand-wrap">
+									<img
+										class="mw-100 logo"
+										src="assets/img/logo.svg"
+										style="
+											width: 400px;
+											height: 90px;
+											object-fit: contain;
+										"
+										alt="Logo"
+										title="Logo"
+									/>
+								</a>
+							</div>
+							<div class="card-body p-4">
+								<h3 class="text-center mb-4 text-white">
+									Login
+								</h3>
+								<form method="POST" action="login" class="px-4">
+									<div class="input-group mb-3">
+										<div
+											class="input-group-text bg-white px-4 py-3 border-end-0"
+										>
+											<img
+												src="assets/img/icons/user.svg"
+												alt="user"
+												class="icon"
+											/>
+										</div>
+										<input
+											id="id"
+											type="text"
+											class="form-control bg-white ps-0 px-4 py-3 border-start-0 shadow-none border"
+											name="id"
+											placeholder="Masukkan ID"
+											required
+										/>
+									</div>
+									<div class="input-group mb-3">
+										<div
+											class="input-group-text bg-white px-4 py-3 border-end-0"
+										>
+											<img
+												src="assets/img/icons/lock.svg"
+												alt="user"
+												class="icon"
+											/>
+										</div>
+										<input
+											id="password"
+											type="password"
+											class="form-control bg-white ps-0 px-4 py-3 border-start-0 shadow-none border"
+											name="password"
+											placeholder="Masukkan Password"
+											required
+										/>
+									</div>
+									<div class="border-top border-bottom p-4">
+										<!-- <button
+										type="submit"
+										class="btn btn-primary btn-block bg-gradient-1 border-0 w-100 py-2 fs-5"
+									>
+										Login
+									</button> -->
+										<a
+											type="button"
+											href="beranda.html"
+											class="btn btn-primary btn-block bg-gradient-1 border-0 w-100 py-2 fs-5"
+										>
+											Login
+										</a>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="assets/js/app.js" type="text/javascript"></script>
+	</body>
 </html>
