0fa5c8e4d9afd8b416d81dbdf5f0e5ec78cd24b0
Rezaldy Rafi <rezaldy.rafi2@gmail.com>
2022-11-12 22:35:32 +0700

    fixing css

--- wbs/resources/views/admin/pengaduan.blade.php
+++ wbs/resources/views/admin/pengaduan.blade.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 		<title>Document</title>
-		<link href="assets/css/app.css" rel="stylesheet" />
+		<link href="assets/css/admin.css" rel="stylesheet" />
 		<!-- Fonts -->
 		<link rel="preconnect" href="https://fonts.googleapis.com" />
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
